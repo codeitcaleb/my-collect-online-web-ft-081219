b44aeddf20acfe5dd94a0b8c693bbc9c70cbbc47
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-18 04:51:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(collection)
+  
+    i = 0 
+    new_collection = []
+    
+    while i < collection.length 
+      new_collection << yield(collection[i])
+      i += 1
+    end  
+    new_collection
+end
 
+my_collect(list)  { |name| name.split(" ").first }
