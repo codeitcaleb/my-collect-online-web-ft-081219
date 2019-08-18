4badee6e0129726f84deeda14990c637063b3a5f
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-18 04:48:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(collection)
+  
+    i = 0 
+    new_collection = []
+    
+    while i < collection.length 
+      new_collection << 
+      new_collection << yield(collection[i])
+      i += 1
+    end  
+    new_collection
+end
 
+my_collect(list)  { |name| name.split(" ").first }
