8f88cd75c5960807d1378e563a0e26e90109f898
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-18 04:50:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,7 +6,6 @@ def my_collect(collection)
     new_collection = []
     
     while i < collection.length 
-      new_collection << 
       new_collection << yield(collection[i])
       i += 1
     end  
